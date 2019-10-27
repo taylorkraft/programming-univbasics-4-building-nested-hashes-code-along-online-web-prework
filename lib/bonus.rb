6a22cb4417403aad3adcc7b77aad9c4f7e381c7e
Taylor Kraft <taylorkraft16@gmail.com>
2019-10-27 22:20:30 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,8 +20,8 @@ def bonus
    }
   }
 
-  epic_tragedy[:montague][:hero][:staus] = "dead"
-  epic_tragedy[:capulet][:heroine][:staus] = "dead"
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
 
   
 
